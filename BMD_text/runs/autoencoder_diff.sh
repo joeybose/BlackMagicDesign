10fda6f7ea5db9b15b7b1a5f6359c1fc94eeb2c4
Andre Cianflone <apcianflone@gmail.com>
2019-05-22 22:34:38 -0400

    run scripts

---
+++ BMD_text/runs/autoencoder_diff.sh
@@ -0,0 +1,22 @@
+# Useful to test adv attacks
+
+cd ..
+python main.py --white \
+    --no_pgd_optim \
+    --hidden_init \
+    --LAMBDA=0.05 \
+    --carlini_loss \
+    --batch_size=128 \
+    --prepared_data='dataloader/128_prepared_data.pickle' \
+    --nearest_neigh_all \
+    --diff_nn \
+    --save_model \
+    --adv_model_path='saved_models/autoencoder_diff.pt' \
+    --comet \
+    --offline_comet \
+    --temp_decay_schedule=25 \
+    --epochs=15 \
+    --seed=1 \
+    --deterministic_G \
+    --namestr="Carlini BMD Text just autoencoder with diff nearest neighbour"
+
