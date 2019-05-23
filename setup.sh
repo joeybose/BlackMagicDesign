6b78d21186d7f1e894f866fed3b156b51934fe8a
Andre Cianflone <apcianflone@gmail.com>
2019-05-22 22:13:54 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+root_dir=$(pwd)
 
+# Install advertorch
 git clone https://github.com/BorealisAI/advertorch
 cd advertorch
 python setup.py install
 cd ..
 rm -rf advertorch
+
+# Install glove
+cd $root_dir
+cd setup_scripts/
+bash download_glove.sh
+
+## Install data
+cd $root_dir
+cd setup_scripts/
+bash download_data.sh
+
+## Download pretrained LSTM
+cd $root_dir
+cd setup_scripts/
+bash download_lstm.sh
